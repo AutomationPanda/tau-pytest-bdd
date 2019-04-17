e475b17b6acb70acbc653034455096d5d0a3e286
AndyLPK247 <AndyLPK247@gmail.com>
2019-04-16 20:44:48 -0400

    added chapter 5 content

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -3,29 +3,40 @@ from pytest_bdd import scenarios, parsers, given, when, then
 from cucumbers import CucumberBasket
 
 
-scenarios('../features/cucumbers.feature')
-
-
 EXTRA_TYPES = {
     'Number': int,
 }
 
 
+CONVERTERS = {
+    'initial': int,
+    'some': int,
+    'total': int,
+}
+
+
+scenarios('../features/cucumbers.feature', example_converters=CONVERTERS)
+
+
 @given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES))
+@given('the basket has "<initial>" cucumbers')
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
 
 @when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES))
+@when('"<some>" cucumbers are added to the basket')
 def add_cucumbers(basket, some):
     basket.add(some)
 
 
 @when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=EXTRA_TYPES))
+@when('"<some>" cucumbers are removed from the basket')
 def remove_cucumbers(basket, some):
     basket.remove(some)
 
 
 @then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES))
+@then('the basket contains "<total>" cucumbers')
 def basket_has_total(basket, total):
     assert basket.count == total
