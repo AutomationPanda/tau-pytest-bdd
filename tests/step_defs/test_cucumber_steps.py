aed8def704760631610213f685344a957ad8d714
Connor Irvine <connor_irvine@hotmail.com>
2022-04-07 10:47:19 +0100

    dictionary tidy up for extra types

--- tests/step_defs/test_cucumber_steps.py
+++ tests/step_defs/test_cucumber_steps.py
@@ -6,18 +6,22 @@ import re
 
 scenarios('../features/cucumbers.feature')
 
-@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=dict(Number=int)), target_fixture="basket")    
+EXTRA_TYPES = {
+    'Number' : int,
+}
+
+@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES), target_fixture="basket")    
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=dict(Number=int)))
+@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES))
 def add_cucumbers(basket, some):
    basket.add(some)
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=dict(Number=int)))
+@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=EXTRA_TYPES))
 def remove_cucumbers(basket, some):
    basket.remove(some)
 
-@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=dict(Number=int)))
+@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES))
 def basket_has_total(basket, total):
     assert basket.count == total
