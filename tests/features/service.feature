65dc2a82619eaf1385b7423ff45bace665ba86ac
Pandy Knight <andylpk.247@gmail.com>
2023-07-19 23:58:36 -0400

    Added service.feature with step defs

---
+++ tests/features/service.feature
@@ -0,0 +1,22 @@
+Feature: DuckDuckGo Instant Answer API
+  As an application developer,
+  I want to get instant answers for search terms via a REST API,
+  So that my app can get answers anywhere.
+
+
+  Scenario Outline: Basic DuckDuckGo API Query
+    Given the DuckDuckGo API is queried with "<phrase>"
+    Then the response status code is "200"
+    And the response contains results for "<phrase>"
+
+    Examples: Animals
+      | phrase   |
+      | panda    |
+      | python   |
+      | platypus |
+
+    Examples: Fruits
+      | phrase     |
+      | peach      |
+      | pineapple  |
+      | papaya     |
---
+++ tests/step_defs/test_service.py
@@ -0,0 +1,53 @@
+"""
+This module contains step definitions for service.feature.
+It uses the requests package:
+http://docs.python-requests.org/
+"""
+
+import requests
+
+from pytest_bdd import scenarios, parsers, given, then
+
+
+# Shared Variables
+
+DUCKDUCKGO_API = 'https://api.duckduckgo.com/'
+
+
+# Scenarios
+
+scenarios('../features/service.feature')
+
+CONVERTERS = {
+  'code': int,
+  'phrase': str,
+}
+
+
+# Given Steps
+
+@given(
+  parsers.parse('the DuckDuckGo API is queried with "{phrase}"'),
+  target_fixture='ddg_response',
+  converters=CONVERTERS)
+def ddg_response(phrase):
+  params = {'q': phrase, 'format': 'json'}
+  response = requests.get(DUCKDUCKGO_API, params=params)
+  return response
+
+
+# Then Steps
+
+@then(
+  parsers.parse('the response contains results for "{phrase}"'),
+  converters=CONVERTERS)
+def ddg_response_contents(ddg_response, phrase):
+  # A more comprehensive test would check 'RelatedTopics' for matching phrases
+  assert phrase.lower() == ddg_response.json()['Heading'].lower()
+
+
+@then(
+  parsers.parse('the response status code is "{code}"'),
+  converters=CONVERTERS)
+def ddg_response_code(ddg_response, code):
+  assert ddg_response.status_code == code
