54deadc2dc76db3c46fd6b5333f0f4d2ee0ccc8d
Pandy Knight <andylpk.247@gmail.com>
2023-07-19 23:49:23 -0400

    Added a scenario outline

--- tests/features/cucumbers.feature
+++ tests/features/cucumbers.feature
@@ -4,10 +4,16 @@ Feature: Cucumber Basket
   So that I don't drop them all.
 
 
-  Scenario: Add cucumbers to a basket
-    Given the basket has "2" cucumbers
-    When "4" cucumbers are added to the basket
-    Then the basket contains "6" cucumbers
+  Scenario Outline: Add cucumbers to a basket
+    Given the basket has "<initial>" cucumbers
+    When "<some>" cucumbers are added to the basket
+    Then the basket contains "<total>" cucumbers
+
+    Examples: Amounts
+      | initial | some | total |
+      | 2       | 4    | 6     |
+      | 0       | 3    | 3     |
+      | 5       | 5    | 10    |
 
 
   Scenario: Remove cucumbers from a basket
--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -6,31 +6,37 @@ from cucumbers import CucumberBasket
 scenarios('../features/cucumbers.feature')
 
 
-EXTRA_TYPES = {
-  'Number': int,
+CONVERTERS = {
+  'initial': int,
+  'some': int,
+  'total': int,
 }
 
 
 @given(
-  parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES),
-  target_fixture='basket')
+  parsers.parse('the basket has "{initial}" cucumbers'),
+  target_fixture='basket',
+  converters=CONVERTERS)
 def basket(initial):
   return CucumberBasket(initial_count=initial)
 
 
 @when(
-  parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES))
+  parsers.parse('"{some}" cucumbers are added to the basket'),
+  converters=CONVERTERS)
 def add_cucumbers(basket, some):
   basket.add(some)
 
 
 @when(
-  parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=EXTRA_TYPES))
+  parsers.parse('"{some}" cucumbers are removed from the basket'),
+  converters=CONVERTERS)
 def remove_cucumbers(basket, some):
   basket.remove(some)
 
 
 @then(
-  parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES))
+  parsers.parse('the basket contains "{total}" cucumbers'),
+  converters=CONVERTERS)
 def basket_has_total(basket, total):
   assert basket.count == total
